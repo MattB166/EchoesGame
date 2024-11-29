1e1e5507e06a5fb959dd5563364734eff47cabbf
MattB166 <matthewbrake2001@hotmail.co.uk>
2024-11-29 23:31:22 +0000

    script setup

---
+++ Echoes Of Time/Assets/Scripts/AI/BaseNPC.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// base class for all NPC's. Both enemies and friendly NPC's(scope permitting) will inherit from this class. 
+/// </summary>
+public class BaseNPC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Echoes Of Time/Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// manages the basic movement of the camera, including perlin noise for camera shake, and camera follow for the player 
+/// </summary>
+public class CameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CamShake()
+    {
+        //perlin noise for camera shake 
+
+    }
+
+    /// <summary>
+    /// generic metroidvania style camera follow for the player 
+    /// </summary>
+    void CamFollow()
+    {
+        //camera follow for the player 
+    } 
+}
