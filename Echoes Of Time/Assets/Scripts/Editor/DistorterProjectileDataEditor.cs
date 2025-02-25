c920eb87dcf570e43c4bff07f62346b98ea9c2c6
MattB166 <matthewbrake2001@hotmail.co.uk>
2025-02-25 12:34:14 +0000

    speed value changes on projectiles

--- Echoes Of Time/Assets/Scripts/Editor/DistorterProjectileDataEditor.cs
+++ Echoes Of Time/Assets/Scripts/Editor/DistorterProjectileDataEditor.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CustomEditor(typeof(DistorterProjectileData))]
+//[CustomEditor(typeof(DistorterProjectileData))]
 public class DistorterProjectileDataEditor : ProjectileDataEditor
 {
 
