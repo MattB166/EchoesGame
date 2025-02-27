7c59e38a74b5860a380ca56a486b523d17c192f7
MattB166 <matthewbrake2001@hotmail.co.uk>
2025-02-27 14:01:49 +0000

    opened a new script for portal and data

---
+++ Echoes Of Time/Assets/Scripts/Items/Portal.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Manages the behavior of the portal item. not collectable, and not interactable by input and cannot be picked up, so
+/// worth having its own individual class. 
+/// </summary>
+public class Portal : MonoBehaviour
+{
+    ///needs its own data set - the portal it will teleport to, time its open for, 
+    ///if applicable, and the time it will take to teleport <summary>
+    /// needs its own data set - the portal it will teleport to, time its open for, 
+    /// </summary>
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
