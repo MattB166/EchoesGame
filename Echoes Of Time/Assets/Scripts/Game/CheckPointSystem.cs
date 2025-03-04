40a09de344bf5617f0f074baf0b583ad4d9d538e
MattB166 <matthewbrake2001@hotmail.co.uk>
2025-03-04 13:02:32 +0000

    checkpoint system class is ready to be written

---
+++ Echoes Of Time/Assets/Scripts/Game/CheckPointSystem.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manages most recent checkpoint, spawning player at that checkpoint. 
+/// </summary>
+public class CheckPointSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
