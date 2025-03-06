61bb2372df7b3494833b033f791a4b3e7942d36c
MattB166 <matthewbrake2001@hotmail.co.uk>
2025-03-06 12:52:57 +0000

    folder clean up and base obj class ready to be populated

---
+++ Echoes Of Time/Assets/Scripts/Game/Objectives/BaseObjective.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Base Objective class for all objectives in the game
+/// </summary>
+public abstract class BaseObjective : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
