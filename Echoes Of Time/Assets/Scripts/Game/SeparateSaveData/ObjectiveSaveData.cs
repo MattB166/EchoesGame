6c23f66c00c45c88d755c2959a306c83ac73d4dc
MattB166 <matthewbrake2001@hotmail.co.uk>
2025-03-12 15:46:45 +0000

    created class for saving objective data, so it can be populated so we can save objectives past and present.

---
+++ Echoes Of Time/Assets/Scripts/Game/SeparateSaveData/ObjectiveSaveData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ObjectiveSaveData 
+{
+    //save all the previously achieved objectives
+    //this is used to prevent the player from achieving the same objective multiple times
+    //save all the current objectives, so we pick up where we left off. 
+}
