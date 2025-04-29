cdfd910a5b24ed68f9d1a0e4d2a3c2fec93a88da
MattB166 <matthewbrake2001@hotmail.co.uk>
2025-04-29 15:31:35 +0100

    spawn manager position changes

--- Echoes Of Time/Assets/Scripts/Game/SpawnManager.cs
+++ Echoes Of Time/Assets/Scripts/Game/SpawnManager.cs
@@ -15,7 +15,7 @@ public class SpawnManager : MonoBehaviour
             DontDestroyOnLoad(gameObject);
             spawnLocations.Add("TestingLevel", new Vector3(-52, -3, 0));
             spawnLocations.Add("PoisonPuzzleLevel", new Vector3(0, 5, 0));
-            spawnLocations.Add("SecondScene", new Vector3(0, 3, 0));
+            spawnLocations.Add("SecondScene", new Vector3(4, 3, 0));
         }
         else
         {
